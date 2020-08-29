2459c3e4564531184123256049d9b5515ce451a9
Russell Hill <6344686+RussDHill@users.noreply.github.com>
2020-08-29 14:02:43 +0100

    Add files via upload

---
+++ HeadFirstLeftoverPatterns/Flyweight/Landscape/MainFrm.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ChildView.h"
+
+class CMainFrame : public CFrameWnd
+{
+	
+public:
+	CMainFrame();
+protected: 
+	DECLARE_DYNAMIC(CMainFrame)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CMainFrame)
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+	CChildView    m_wndView;
+
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CMainFrame)
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
+		// NOTE - the ClassWizard will add and remove member functions here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
